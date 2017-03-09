9373e61ef3e19cfdec0ec56df97be8e9fcfe7273
Geoffrey Muller <geoffrey@ambivorous.com>
2017-03-09 13:10:05 +0200

    Version database changes.

--- data/update_database.js
+++ data/update_database.js
@@ -4,24 +4,22 @@ var db = new sqlite3.Database('./data/table_tennis.db');
 module.exports = function() {
     db.serialize(function() {
         var version;
+        var latest = 1.0;
         var query = "BEGIN TRANSACTION; ";
 
-        db.run("CREATE TABLE IF NOT EXISTS version (key TEXT PRIMARY KEY UNIQUE, version REAL)");
-        db.run("INSERT INTO version(key, version) VALUES(\"database\", 1.0)");
-
-        /*db.get("SELECT version FROM version WHERE key = ?", [ "database" ], function(err, row) {
+        db.get("SELECT version FROM version WHERE key = ?", [ "database" ], function(err, row) {
             if (row.version) {
                 version = row.version;
             } else {
                 // no database version yet (log file)
             }
 
-            if (version < 1.0) {
+            if (version < latest) {
                 // EXAMPLE: update to 1.0
             }
             query += "COMMIT";
 
-            if (version == 1.0) {
+            if (version < latest) {
                 db.exec(query, function(err) {
                     if (err) {
                         // fuck, i need a logfile
@@ -33,7 +31,7 @@ module.exports = function() {
             } else {
                 // database is up to date (log file)
             }
-        });*/
+        });
     });
 
     db.close();
