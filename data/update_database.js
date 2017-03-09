2598646415e4f7e3d0fc6503157a5ab8805e5911
Geoffrey Muller <geoffrey@ambivorous.com>
2017-03-09 13:20:17 +0200

    Tweaks to update database.

--- data/update_database.js
+++ data/update_database.js
@@ -2,37 +2,34 @@ var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('./data/table_tennis.db');
 
 module.exports = function() {
-    db.serialize(function() {
-        var version;
-        var latest = 1.0;
-        var query = "BEGIN TRANSACTION; ";
+    var version;
+    var latest = 1.0;
+    var query = "BEGIN TRANSACTION; ";
 
-        db.get("SELECT version FROM version WHERE key = ?", [ "database" ], function(err, row) {
-            if (row.version) {
-                version = row.version;
-            } else {
-                // no database version yet (log file)
-            }
+    db.get("SELECT version FROM version WHERE key = ?", [ "database" ], function(err, row) {
+        if (row.version) {
+            version = row.version;
+        } else {
+            // no database version yet (log file)
+        }
 
-            if (version < latest) {
-                // EXAMPLE: update to 1.0
-            }
-            query += "COMMIT";
+        if (version < latest) {
+            // EXAMPLE: update to latest
+            query += "UPDATE version SET version = " + latest + " WHERE key = 'database'; ";
+        }
+        query += "COMMIT";
 
-            if (version < latest) {
-                db.exec(query, function(err) {
-                    if (err) {
-                        // fuck, i need a logfile
-                        return;
-                    }
+        if (version < latest) {
+            db.exec(query, function(err) {
+                if (err) {
+                    // fuck, i need a logfile
+                    return;
+                }
 
-                    // woo, updated? seriously; log file
-                });
-            } else {
-                // database is up to date (log file)
-            }
-        });
+                // woo, updated? seriously; log file
+            });
+        } else {
+            // database is up to date (log file)
+        }
     });
-
-    db.close();
 };
