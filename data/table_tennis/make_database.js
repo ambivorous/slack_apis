6fbc8322fc74548d8241f46d8ceaf6a6712ec1c6
Daniel Browne <daniel.browne@voss-solutions.com>
2017-03-15 12:34:17 +0200

    my first commit :D

--- data/table_tennis/make_database.js
+++ data/table_tennis/make_database.js
@@ -6,7 +6,7 @@ db.serialize(function() {
     db.run("CREATE TABLE IF NOT EXISTS match (match_id INTEGER PRIMARY KEY UNIQUE, winner INTEGER REFERENCES user(user_id), winner_wins INTEGER, "
         + "loser INTEGER REFERENCES user(user_id), loser_wins INTEGER, date INTEGER, ranking_change INTEGER)");
     db.run("CREATE TABLE IF NOT EXISTS version (key TEXT PRIMARY KEY UNIQUE, version FLOAT)");
-    db.run("INSERT INTO version (key, version) VALUES (\"database\", 1.0)");
+    db.run("INSERT INTO version (key, version) VALUES (\"database\", 1.1)");
 });
 
 db.close();
