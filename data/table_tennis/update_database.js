8b36bd3c6dd1775b1e98777691140f7004b18b24
Geoffrey Muller <geoffrey@ambivorous.com>
2017-03-20 20:44:23 +0200

    Made database creation incremental. T_T

--- data/table_tennis/update_database.js
+++ data/table_tennis/update_database.js
@@ -1,4 +1,7 @@
-const LATEST = 1.2;
+const VER_1_0 = 1.0;
+const VER_1_1 = 1.1;
+const VER_1_2 = 1.2;
+const LATEST = VER_1_2;
 
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('./data/table_tennis.db');
@@ -8,9 +11,21 @@ module.exports = function() {
     var query = "";
 
     db.get("SELECT version FROM version WHERE key = ?", [ "database" ], function(err, row) {
-        version = row.version;
+        if (err) {
+            version = 1.0;
 
-        if (version < 1.1) {
+            query += "BEGIN TRANSACTION; ";
+            query += "CREATE TABLE IF NOT EXISTS player (username TEXT PRIMARY KEY UNIQUE, ranking INTEGER); ";
+            query += "CREATE TABLE IF NOT EXISTS match (match_id INTEGER PRIMARY KEY AUTOINCREMENT, winner TEXT REFERENCES player (username), winner_wins INTEGER, "
+                + "loser TEXT REFERENCES player (username), loser_wins INTEGER, date INTEGER, ranking_change INTEGER); ";
+            query += "CREATE TABLE IF NOT EXISTS version (key TEXT PRIMARY KEY UNIQUE, version FLOAT); ";
+            query += "INSERT INTO version(key, version) VALUES(\"database\", " + VER_1_0 + "); ";
+            query += "COMMIT; ";
+        } else {
+            version = row.version;
+        }
+
+        if (version < VER_1_1) {
             query += "BEGIN TRANSACTION; ";
             query += "CREATE TABLE IF NOT EXISTS user (user_id INTEGER PRIMARY KEY UNIQUE, username TEXT UNIQUE, nickname TEXT, ranking INTEGER); ";
             query += "INSERT INTO user (user_id, username, ranking) SELECT rowid, username, ranking FROM player; ";
@@ -27,13 +42,14 @@ module.exports = function() {
                 + "SELECT match_id, winner_id, winner_wins, loser_id, loser_wins, date, ranking_change FROM match_orig; ";
             query += "DROP TABLE match_orig; ";
 
-            query += "UPDATE version SET version = " + 1.1 + " WHERE key = 'database'; ";
+            query += "UPDATE version SET version = " + VER_1_1 + " WHERE key = 'database'; ";
             query += "COMMIT; ";
         }
 
-        if (version < 1.2) {
+        if (version < VER_1_2) {
             query += "BEGIN TRANSACTION; ";
             query += "CREATE TABLE IF NOT EXISTS reservations (pkid INTEGER PRIMARY KEY UNIQUE, start_timestamp INTEGER, end_timestamp INTEGER, username TEXT); ";
+            query += "UPDATE version SET version = " + VER_1_2 + " WHERE key = 'database'; ";
             query += "COMMIT; ";
         }
 
